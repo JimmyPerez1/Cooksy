cf359da17d95561c9c6837ff76237f9da21f9759
JimmyPerez1 <yimianperez@gmail.com>
2025-05-20 16:29:40 -0400

    Users Show Functionality

--- controllers/users.js
+++ controllers/users.js
@@ -2,12 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 const User = require('../models/user.js');
+const Recipe = require('../models/recipe.js');
 
-// Get users
+// Get /users
 router.get('/', async (req, res) => {
   const users = await User.find({})
     res.render('users/index.ejs', {users})
 })
+
+// Show users
+router.get('/:id', async (req, res) => {
+  const user = await User.findById(req.params.id);
+  const recipes = await Recipe.find({ createdBy: user._id}) 
+  res.render('users/show.ejs', { user, recipes });
+})
+
 
 
 module.exports = router;
